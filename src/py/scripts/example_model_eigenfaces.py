a605352f8088d96f5438aa15f2c966bab81d7208
y0ngSheng <37537690+y0ngSheng@users.noreply.github.com>
2020-04-30 18:23:25 +0800

    Update example_model_eigenfaces.py

--- src/py/scripts/example_model_eigenfaces.py
+++ src/py/scripts/example_model_eigenfaces.py
@@ -10,7 +10,7 @@ from tinyfacerec.model import EigenfacesModel
 if __name__ == '__main__':
 
     if len(sys.argv) != 2:
-        print "USAGE: example_model_eigenfaces.py </path/to/images>"
+        print ("USAGE: example_model_eigenfaces.py </path/to/images>")
         sys.exit()
     
     # read images
